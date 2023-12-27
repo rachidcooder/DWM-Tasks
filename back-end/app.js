7e6715c7be61ed1f9c6840e8b10ad3a7b8430f06
rachidcooder <cooderherou@gmail.com>
2023-12-27 23:12:36 +0000

    back to deploy

--- back-end/app.js
+++ back-end/app.js
@@ -1,21 +1,21 @@
-import express from "express"
+import express from 'express'
 import dotenv from 'dotenv'
 import UserRoute from './Routers/UserRoute.js'
 import TaskRoute from './Routers/TaskRoute.js'
 import cors from 'cors'
 import ConnectDB from "./configs/db.js"
 dotenv.config();
-const app =express();
-const PORT=process.env.PORT||3001;
+const app = express();
+const PORT = process.env.PORT || 3001;
 
- app.use(express.json());
- app.use(cors());
+app.use(express.json());
+app.use(cors());
 
-  ConnectDB();
+ConnectDB();
 
-  app.use('/api/user',UserRoute);
-  app.use('/api/task',TaskRoute);
+app.use('/api/user', UserRoute);
+app.use('/api/task', TaskRoute);
 
-app.listen(PORT,()=>{
-  console.log("Listening to Port :",PORT);
+app.listen(PORT, () => {
+  console.log("Listening to Port :", PORT);
 })
