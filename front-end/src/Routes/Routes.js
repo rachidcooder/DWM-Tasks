faafb7218e741249b472c10a522dcbd760b6733c
rachidcooder <cooderherou@gmail.com>
2024-01-05 20:37:36 +0000

    link to Api

--- front-end/src/Routes/Routes.js
+++ front-end/src/Routes/Routes.js
@@ -1,10 +1,10 @@
-const host ='http://localhost:3000/api'
+const host = 'https://onlay-shop-api.onrender.com/api'
 
-export const signInRoute=`${host}/user/sign-in`;
-export const loginRoute=`${host}/user/login`;
+export const signInRoute = `${host}/user/sign-in`;
+export const loginRoute = `${host}/user/login`;
 
-export const AddTskRoute=`${host}/task/add`;
-export const updatTskeRoute=`${host}/task/update`;
-export const deleteTskRoute=`${host}/task/delete`;
-export const getTskRoute=`${host}/task/get`;
+export const AddTskRoute = `${host}/task/add`;
+export const updatTskeRoute = `${host}/task/update`;
+export const deleteTskRoute = `${host}/task/delete`;
+export const getTskRoute = `${host}/task/get`;
 
